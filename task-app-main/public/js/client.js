4a32e94da9ac25c6a5ae134954b2de8295f5e1a5
Sumit80076 <91448090+Sumit80076@users.noreply.github.com>
2023-07-08 15:03:02 +0530

    Delete task-app-main directory

--- task-app-main/public/js/client.js
+++
@@ -1,9 +0,0 @@
-//Load Client side JavaScript
-console.log("JavaScript Loaded!")
-
-
-function checkValue()
-{
-  var checkbox = document.getElementById('checkbox');
-  checkbox.value = checkbox.checked
-}
--- task-app-main/src/db/mongoose.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-
-const connectionURL = process.env.MONGODB_URL_DB 
-
-mongoose.connect(connectionURL, {})
-
